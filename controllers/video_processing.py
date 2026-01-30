32d9931d4089cc2a2eb9cf0e85fca70b54d95edc
Ahmed Karem Mousa <59775002+AhmedMousa-ag@users.noreply.github.com>
2026-01-30 12:50:23 -0600

    feat(video): annotate output video (#2)
    * feat(video): annotate output video
    
    * fix(video annotation): fix errors types

--- controllers/video_processing.py
+++ controllers/video_processing.py
@@ -3,26 +3,34 @@ from controllers.yolo import get_key_points
 from models.video_models import VideoResult, VideoMetadata, FrameMetadata
 from typing import List
 import time
+from configs.config import SKELETON_CONNECTIONS
 
 
-def process_video(video_path: str) -> VideoResult:
+# NOTE: The only reason I combined the logic of extracting pose estimation and writing a video in the same place is to be able to calculate the frame average time, otherwise I would have splitted it.
+def process_video(video_path: str, output_video_path) -> VideoResult:
     video = cv2.VideoCapture(video_path)
     if not video.isOpened():
-        raise ValueError(
+        raise FileNotFoundError(
             f"Faild to open video at path: {video_path}, please check if the video exists"
         )
     video_metadata = __extract_video_metadata(video)
+    output_video_fbs = video_metadata.fps
     skip_frames = False
     if (
         video_metadata.fps > 30
     ):  # for optimization, no need to process more than 30 frames per second.
         print(f"FPS: {video_metadata.fps} and will skip half these frames.")
         skip_frames = True
-
+        output_video_fbs /= 2
+    output_video = cv2.VideoWriter(
+        output_video_path,
+        cv2.VideoWriter_fourcc(*"mp4v"),
+        output_video_fbs,
+        (video_metadata.width, video_metadata.height),
+    )
     frame_count = 0
     frames_metadata: List = []
-    avg_frame_proc_time = 0.0
-    calculated_frame_time_count = 1
+    frame_times = []
     while video.isOpened():
         if (
             skip_frames and frame_count % 2
@@ -37,21 +45,23 @@ def process_video(video_path: str) -> VideoResult:
         frame_count += 1
         timestamp_ms = video.get(cv2.CAP_PROP_POS_MSEC)
         keypoints, pose_score = get_key_points(img)
-        frames_metadata.append(
-            FrameMetadata(
-                frame_index=frame_count,
-                timestamp_ms=int(timestamp_ms),
-                pose_score=pose_score,
-                keypoints=keypoints,
-            )
+        frame_metadata = FrameMetadata(
+            frame_index=frame_count,
+            timestamp_ms=int(timestamp_ms),
+            pose_score=pose_score,
+            keypoints=keypoints,
         )
+        frames_metadata.append(frame_metadata)
+        output_video.write(annotate_frame(img, frame_metadata))
         end_time = time.time()
-        avg_frame_proc_time += (
-            end_time - start_time
-        ) / calculated_frame_time_count  # Guranteed to be 1 at least.
-        calculated_frame_time_count += 1
-    print(f"Processed frame time average is: {avg_frame_proc_time:.4f} seconds ")
-
+        frame_times.append(end_time - start_time)
+    print(
+        f"Processed frame time average is: {sum(frame_times)/len(frame_times):.4f} seconds "
+    )
+    # 5. Clean up
+    video.release()
+    output_video.release()
+    cv2.destroyAllWindows()
     return VideoResult(meta=video_metadata, frames=frames_metadata)
 
 
@@ -61,3 +71,30 @@ def __extract_video_metadata(cap: cv2.VideoCapture) -> VideoMetadata:
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     return VideoMetadata(fps=fps, width=width, height=height, frame_count=frame_count)
+
+
+def annotate_frame(frame, frame_metadata: FrameMetadata):
+    # 1. Create a dictionary to look up KeyPoint objects by their name
+    keypoint_map = {kp.name: kp for kp in frame_metadata.keypoints}
+
+    # 2. Draw Skeleton Connections first (so lines are behind the dots)
+    for start_name, end_name in SKELETON_CONNECTIONS:
+        if start_name in keypoint_map and end_name in keypoint_map:
+            kp_start = keypoint_map[start_name]
+            kp_end = keypoint_map[end_name]
+
+            # Convert to int for OpenCV
+            start_point = (int(kp_start.x), int(kp_start.y))
+            end_point = (int(kp_end.x), int(kp_end.y))
+
+            # Draw the line
+            cv2.line(frame, start_point, end_point, (0, 0, 255), 2)
+
+    # 3. Draw Keypoints (Joints)
+    for keypoint in frame_metadata.keypoints:
+        x, y = int(keypoint.x), int(keypoint.y)
+
+        # Draw the circle
+        cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+    return frame
